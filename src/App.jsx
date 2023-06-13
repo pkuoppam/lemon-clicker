c38b408523c4cae08ef87d811df7cfb965fcb994
pkuoppam <petri.kuoppamaa@edu.sasky.fi>
2023-06-13 23:07:03 +0300

    lisää Header-komponentin

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,22 @@ import Balance from './components/Balance';
 import Lemon from './components/Lemon';
 import './App.css'
 import Booster from './components/Booster';
+import Header from './components/Header';
+
 
 function App() {
 
   return (
     <>
       <div>
+        <Header>lemon clicker</Header>
         <Balance total="157" />
         <Lemon />
         <Booster value="3.2" />
       </div>  
     </>
   )
+
 
 
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+function Header(props) {
+
+    return (
+      <div className="header">
+        <h1>{props.children}</h1>
+      </div>
+    );
+  
+  }
+  
+  export default Header;
+  
