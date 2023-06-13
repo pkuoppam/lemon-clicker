e91abc5e6b258a848a79da804528ad9dd20d0b27
pkuoppam <petri.kuoppamaa@edu.sasky.fi>
2023-06-13 23:29:18 +0300

    muuttaa Header-komponentin toiminnan

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,22 @@
-function Header(props) {
+import iconLemon from '../assets/lemon.svg';
 
-    return (
-      <div className="header">
-        <h1>{props.children}</h1>
-      </div>
-    );
-  
-  }
+
+function Header(props) {
+    if (props.hasOwnProperty("balance")) {
+        return (
+          <div className="header header_sub">
+            <h1>{props.children}</h1>
+            <div>{props.balance} <img src={iconLemon}/></div>
+          </div>
+        );
+      } else {
+        return (
+          <div className="header">
+            <h1>{props.children}</h1>
+          </div>
+        );
+      }
+    }    
   
   export default Header;
   
